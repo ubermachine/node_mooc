796357eb15b5c8aa3816bc9cee4cbb9273ddc671
ubermachine <shivamkatoch@outlook.com>
2020-07-05 19:51:56 +0530

    mrmongoose pt3

--- mrmongoose/index.js
+++ mrmongoose/index.js
@@ -5,21 +5,38 @@ const Dishes = require('./models/dishes');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url, {useNewUrlParser: true,useUnifiedTopology: true})
 mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+     Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
     })
     .then((dish) => {
         console.log(dish);
-        
-        return Dishes.find({}).exec();
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
 
         return Dishes.deleteMany({});
     })
@@ -29,6 +46,7 @@ connect.then((db) => {
     .catch((err) => {
         console.log(err);
     });
+    
     
 
 });
