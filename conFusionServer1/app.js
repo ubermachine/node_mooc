b154b671ba55f70f0cc012ba51ddab8af1de2613
ubermachine <shivamkatoch@outlook.com>
2020-07-06 23:43:56 +0530

    week 3 basic auth

--- conFusionServer1/app.js
+++ conFusionServer1/app.js
@@ -11,18 +11,24 @@ var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
 
+const mongoose = require("mongoose");
 
+const Dishes = require("./models/dishes");
 
-const mongoose = require('mongoose');
+const url = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const Dishes = require('./models/dishes');
-
-const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url,{ useNewUrlParser: true,useUnifiedTopology: true });
-
-connect.then((db) => {
+connect.then(
+  (db) => {
     console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -33,6 +39,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+function auth(req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == "admin" && pass == "password") {
+    next(); // authorized
+  } else {
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+  }
+}
+
+app.use(auth);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
