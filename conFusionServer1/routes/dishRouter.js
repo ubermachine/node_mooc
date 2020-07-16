e5107ab2d65a123434daa481391524fd9bffae64
ubermachine <shivamkatoch@outlook.com>
2020-07-16 21:50:06 +0530

    Left unecessary fb module

--- conFusionServer1/routes/dishRouter.js
+++ conFusionServer1/routes/dishRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-var cors = require("cors");
+
 const Dishes = require("../models/dishes");
 const cors = require("./cors");
 const dishRouter = express.Router();
